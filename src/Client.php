f70ceafd8b4492a1e5e3040015f46b58fb4e9366
Madan Sapkota <code@madansapkota.com>
2019-12-20 19:02:54 +0545

    create method to handle http request.

--- src/Client.php
+++ src/Client.php
@@ -18,11 +18,6 @@ class Client
     protected $baseUrl;
 
     /**
-     * @var \GuzzleHttp\Client $client
-     */
-    protected $client;
-
-    /**
      * @var array $authorization
      */
     protected $authorization;
@@ -35,7 +30,7 @@ class Client
     {
         // token required for authorization
         if (isset($configs['token']) && !empty($configs['token'])) {
-            $this->authorization = ['token' => $configs['token']];
+            $this->authorization = array('token' => $configs['token']);
         } else {
             throw new SaralSMSException('The API token is required.');
         }
@@ -46,10 +41,60 @@ class Client
         } else {
             $this->baseUrl = $this->liveUrl;
         }
+    }
 
-        // init guzzle client
-        $this->client = new \GuzzleHttp\Client([
-            'base_uri' => $this->baseUrl
-        ]);
+    /**
+     * --------------------------------------------------
+     * create cURL client and make the http request.
+     * --------------------------------------------------
+     * @param $method
+     * @param $route
+     * @param array $params
+     * @return string
+     * @throws SaralSMSException
+     * --------------------------------------------------
+     */
+    protected function request($method, $route, $params = array())
+    {
+        // create a new cURL resource
+        $curl = curl_init();
+
+        // init default headers
+        $headers = array('Accept', 'application/json');
+
+        if ($method === 'POST') {
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+            curl_setopt($curl, CURLOPT_URL, $this->baseUrl . $route);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($params));
+
+            // custom headers
+            $headers['Content-Type'] = array('Content-Type: application/json');
+        } else {
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
+            curl_setopt($curl, CURLOPT_URL, $this->baseUrl . $route . '?' . http_build_query($params));
+        }
+
+        // set options
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($curl, CURLOPT_ENCODING, '');
+        curl_setopt($curl, CURLOPT_MAXREDIRS, 10);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 60);
+
+        // apply headers
+        curl_setopt($curl, CURLOPT_HTTPHEADER, implode(': ', $headers));
+
+        // grab the content
+        $response = curl_exec($curl);
+
+        // handle errors
+        if ($response === false) {
+            throw new SaralSMSException(curl_error($curl));
+        }
+
+        // close cURL resource
+        curl_close($curl);
+        return $response;
     }
 }
