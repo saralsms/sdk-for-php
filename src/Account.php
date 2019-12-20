f560d090f7b1fb900a19807fc4d4c1ac75723d1d
Madan Sapkota <code@madansapkota.com>
2019-12-20 17:13:30 +0545

    create method to get user profile.

--- src/Account.php
+++ src/Account.php
@@ -4,8 +4,27 @@ final class Account extends Client
 {
     /**
      * --------------------------------------------------
+     * This will return the use profile on behalf of
+     * a authenticated token.
+     * --------------------------------------------------
+     * @return object
+     * @throws SaralSMSException
+     * --------------------------------------------------
+     */
+    public function profile(): object
+    {
+        try {
+            $request = $this->client->request('GET', '/account/profile?' . http_build_query($this->authorization));
+            return json_decode($request->getBody()->getContents(), false);
+        } catch (Exception $e) {
+            throw new SaralSMSException($e->getMessage(), $e->getCode());
+        }
+    }
+
+    /**
+     * --------------------------------------------------
      * This will return the account balance on behalf of
-     * a authenticated user/token.
+     * a authenticated token.
      * --------------------------------------------------
      * @return object
      * @throws SaralSMSException
