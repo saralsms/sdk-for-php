28034e6ebb7a8f3ef5836928f85fafa3d3e39541
Madan Sapkota <code@madansapkota.com>
2019-12-20 17:46:51 +0545

    placeholder methods for report.

---
+++ src/Report.php
@@ -0,0 +1,19 @@
+<?php
+
+final class Report extends Client
+{
+    public function getReportById()
+    {
+
+    }
+
+    public function getReportByIdentifier()
+    {
+
+    }
+
+    public function getReports()
+    {
+
+    }
+}
