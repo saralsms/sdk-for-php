c327564b57751f54b60cc17d49bda580009bed60
Madan Sapkota <code@madansapkota.com>
2020-04-05 15:58:31 +0545

    Fix autoloader path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,4 +6,4 @@ error_reporting(-1);
 date_default_timezone_set('UTC');
 
 // include the composer autoloader
-require __DIR__ . '/../vendor/autoload.php';
+require_once dirname(__DIR__) . '/vendor/autoload.php';
