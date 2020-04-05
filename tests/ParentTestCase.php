fb93743d928efa0ffebcfd14a90d37d633f08b17
Madan Sapkota <code@madansapkota.com>
2020-04-05 19:45:28 +0545

    Set API token from ENV

--- tests/ParentTestCase.php
+++ tests/ParentTestCase.php
@@ -3,8 +3,33 @@
 namespace Tests;
 
 use PHPUnit\Framework\TestCase;
+use SaralSMS\Client;
+use SaralSMS\Exception\SaralSMSException;
 
 class ParentTestCase extends TestCase
 {
+    /**
+     * @var Client $client
+     */
+    protected Client $client;
 
+    /**
+     * @throws SaralSMSException
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        if (getenv('SARALSMS_API_TOKEN')) {
+            $token = getenv('SARALSMS_API_TOKEN');
+        } else {
+            $token = $_SERVER['SARALSMS_API_TOKEN'];
+        }
+
+        // init the client
+        $this->client = new Client([
+            'token' => $token,
+            'is_sandbox' => true,
+        ]);
+    }
 }
