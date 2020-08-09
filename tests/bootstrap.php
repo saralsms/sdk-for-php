a31cc03562b07db37402dcc7163eb4d8d875e0b9
Madan Sapkota <code@madansapkota.com>
2020-08-10 01:56:46 +0545

    Read env file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,5 +2,9 @@
 // default timezone
 date_default_timezone_set('UTC');
 
-// include the composer autoloader
+// include autoloader
 require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+// read env
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__), '.env.default');
+$dotenv->load();
