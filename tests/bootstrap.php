eb73099462df1f1a4668175140d122d5a7376f2a
Madan Sapkota <code@madansapkota.com>
2020-08-10 02:07:16 +0545

    Use default file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,5 +6,5 @@ date_default_timezone_set('UTC');
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 // read env
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__), '.env.default');
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
