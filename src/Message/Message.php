2f533da3d8d81d29da23e501bb97cd0ed8fb4a99
Madan Sapkota <code@madansapkota.com>
2020-08-10 10:43:40 +0545

    Create method to send message

--- src/Message/Message.php
+++ src/Message/Message.php
@@ -2,43 +2,29 @@
 
 namespace SaralSMS\Message;
 
-use Exception;
-use SaralSMS\Exception\SaralSMSException;
-
 trait Message
 {
     /**
-     * This will send the message to single number.
+     * This will send the message to single or multiple
+     * numbers and return status.
      *
-     * @param string $number
+     * @param array $numbers
      * @param string $message
      *
      * @return object
-     * @throws SaralSMSException
      */
-    public function sendMessage($number, $message)
+    public function send(array $numbers, string $message)
     {
-        // numbers array is required
-        if (empty($number)) {
-            throw new SaralSMSException('The number param is required.');
-        }
-
-        // message is required
-        if (empty($message)) {
-            throw new SaralSMSException('The message param is required.');
-        }
-
-        // init required params
+        // format params
         $params = [
-            'recipients' => [$number],
-            'body' => $message,
+            'to' => implode(',', $numbers),
+            'text' => $message,
         ];
 
-        try {
-            $request = $this->request('POST', 'messages/send-bulk-sms', $params);
-            return json_decode($request, false);
-        } catch (Exception $e) {
-            throw new SaralSMSException($e->getMessage(), $e->getCode());
-        }
+        $response = $this->client
+            ->request('POST', '/v1/send', [
+                'json' => $params,
+            ]);
+        return json_decode($response->getBody(), false);
     }
 }
